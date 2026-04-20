581c71e11ec0807e0f248e61e27517e67f362377
CaptainDread_ <47885273+HMG-CaptainDread@users.noreply.github.com>
2026-04-20 07:45:41 +0100

    [MERGE] Furniture Module (#18)
    decided to rip out the majority of shit so this isn't a behemoth of a mod to boot every time, will clean up more later

--- .source/Dump Files/Assortment/Ancients/Core/Source/VFEAncients/Comps/CompProperties_Facility_PowerUnlock.cs
+++
@@ -1,9 +0,0 @@
-﻿using RimWorld;
-
-namespace VFEAncients
-{
-    public class CompProperties_Facility_PowerUnlock : CompProperties_Facility
-    {
-        public int unlockedLevels;
-    }
-}
--- .source/Dump Files/Assortment/Progression/Storytellers/Source/HotSwappableAttribute.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace ProgressionStorytellers
-{
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct)]
-    public class HotSwappableAttribute : Attribute
-    {
-    }
-}
